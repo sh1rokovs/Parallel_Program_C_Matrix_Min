8a11982b90527ce712625e7e90537e851f794cbd
Alexandra Tyurmina <sasha.tyurmina@gmail.com>
2020-12-16 12:54:01 +0300

    checking

--- modules/task_3/tyurmina_a_optimization/main.cpp
+++ modules/task_3/tyurmina_a_optimization/main.cpp
@@ -17,20 +17,18 @@ TEST(global_GlobalOpt, TEST0_seq_min) {
     GlobalOpt opt(0, 2.2, f_test1, 1e-5);
 
     double result = opt.GlobalSearchSeq(800);
-    double correct_result = 1.05298;
+    double correct_result = 1.0529;
     ASSERT_NEAR(result, correct_result, 1e-3);
 }
 
 TEST(global_GlobalOpt, TEST1_par_min) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    GlobalOpt opt(0.3, 2.2, f_test2, 1e-5);
+    GlobalOpt opt(0.3, 2.2, f_test1, 1e-5);
 
     double result = opt.GlobalSearchPar(800);
-    double correct_result = 0.3;
-    if (rank == 0) {
-        ASSERT_NEAR(result, correct_result, 1e-4);
-        }
+    double correct_result = 1.05228;
+    if (rank == 0) ASSERT_NEAR(result, correct_result, 1e-4);
 }
 
 TEST(global_GlobalOpt, TEST2_parallel_min_for_f) {
@@ -40,19 +38,21 @@ TEST(global_GlobalOpt, TEST2_parallel_min_for_f) {
 
     double result = opt.GlobalSearchPar(800);
     double correct_result = 0;
-    if (rank == 0) {
-        ASSERT_NEAR(result, correct_result, 1e-5);
-    }
+    if (rank == 0) ASSERT_NEAR(result, correct_result, 1e-5);
 }
 
 TEST(global_GlobalOpt, TEST3_a_greater_then_b_par) {
     GlobalOpt opt(2.2, 0, f_test1, 1e-5);
-    EXPECT_ANY_THROW(opt.GlobalSearchPar(800));
+    double result = 0;
+
+    EXPECT_ANY_THROW(result = opt.GlobalSearchPar(800));
 }
 
 TEST(global_GlobalOpt, TEST4_a_greater_then_b_seq) {
     GlobalOpt opt(2.2, 0, f_test1, 1e-5);
-    EXPECT_ANY_THROW(opt.GlobalSearchSeq(800));
+    double result = 0;
+
+    EXPECT_ANY_THROW(result = opt.GlobalSearchSeq(800));
 }
 
 int main(int argc, char** argv) {
