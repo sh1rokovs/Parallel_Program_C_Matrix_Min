bb8735f1b94331e96b05f42d44a2497cbafcdcea
Sergey01923 <s.bessolitsyn@mail.ru>
2020-12-20 17:28:57 +0300

    12

--- modules/task_3/bessolitsyn_s_gauss/main.cpp
+++ modules/task_3/bessolitsyn_s_gauss/main.cpp
@@ -6,11 +6,34 @@
 #include <vector>
 #include "./bessolitsyn_s_gauss.h"
 
-TEST(Parallel_Operations_MPI, Test_100) {
+TEST(Parallel_Operations_MPI, Test_50) {
     MPI_Barrier(MPI_COMM_WORLD);
     int p_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &p_rank);
-    int n = 100;
+    int n = 50;
+    double t;
+    std::vector<double> input_image, s_output_image, p_output_image;
+    if (p_rank == 0) {
+        input_image.resize(n*n);
+        for (int i = 0; i < n * n; i++)
+            input_image[i] = i % 10;
+        t = MPI_Wtime();
+    }
+    p_output_image = filter_par(input_image, n, n);
+    if (p_rank == 0) {
+        std::cout << "Parallel filter:" << MPI_Wtime() - t << std::endl;
+        t = MPI_Wtime();
+        s_output_image = filter_seq(input_image, n, n);
+        std::cout << "Sequential filter:" << MPI_Wtime() - t << std::endl;
+        ASSERT_NO_THROW();
+    }
+}
+
+TEST(Parallel_Operations_MPI, Test_250) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    int p_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &p_rank);
+    int n = 250;
     double t;
     std::vector<double> input_image, s_output_image, p_output_image;
     if (p_rank == 0) {
@@ -52,11 +75,11 @@ TEST(Parallel_Operations_MPI, Test_500) {
     }
 }
 
-TEST(Parallel_Operations_MPI, Test_1000) {
+TEST(Parallel_Operations_MPI, Test_2500) {
     MPI_Barrier(MPI_COMM_WORLD);
     int p_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &p_rank);
-    int n = 1000;
+    int n = 2500;
     double t;
     std::vector<double> input_image, s_output_image, p_output_image;
     if (p_rank == 0) {
@@ -80,29 +103,6 @@ TEST(Parallel_Operations_MPI, Test_5000) {
     int p_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &p_rank);
     int n = 5000;
-    double t;
-    std::vector<double> input_image, s_output_image, p_output_image;
-    if (p_rank == 0) {
-        input_image.resize(n*n);
-        for (int i = 0; i < n * n; i++)
-            input_image[i] = i % 10;
-        t = MPI_Wtime();
-    }
-    p_output_image = filter_par(input_image, n, n);
-    if (p_rank == 0) {
-        std::cout << "Parallel filter:" << MPI_Wtime() - t << std::endl;
-        t = MPI_Wtime();
-        s_output_image = filter_seq(input_image, n, n);
-        std::cout << "Sequential filter:" << MPI_Wtime() - t << std::endl;
-        ASSERT_NO_THROW();
-    }
-}
-
-TEST(Parallel_Operations_MPI, Test_10000) {
-    MPI_Barrier(MPI_COMM_WORLD);
-    int p_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &p_rank);
-    int n = 10000;
     double t;
     std::vector<double> input_image, s_output_image, p_output_image;
     if (p_rank == 0) {
