047e237f0fd8fb8eb1a678e384f34ba72178b5ce
Andrey0948 <andrey0948@mail.ru>
2020-12-26 20:07:11 +0300

    fix

--- modules/task_2/gapon_a_gather/gather.h
+++ modules/task_2/gapon_a_gather/gather.h
@@ -4,10 +4,10 @@
 
 #include <vector>
 
-int Gather(void* sbuf, int scount, MPI_Datatype stype,
-    void* rbuf, int rcount, MPI_Datatype rtype, int root, MPI_Comm comm);
-std::vector<int> getRandomArrInt(int number, int rank);
-std::vector<float> getRandomArrFloat(int number, int rank);
-std::vector<double> getRandomArrDouble(int number, int rank);
+int Gather(void* sendbuf, int sendcount, MPI_Datatype sendtype,
+    void* recbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm);
+std::vector<int> ArrInt(int num);
+std::vector<float> ArrFloat(int num);
+std::vector<double> ArrDouble(int num);
 
 #endif  // MODULES_TASK_2_GAPON_A_GATHER_GATHER_H_
